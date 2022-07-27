f93efb444523bb0e0d75f8fbfa4cf820bc6f6272
Your Name <you@example.com>
2022-07-27 23:40:18 +0100

    wip

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,30 @@ USE employee_db;
 
 // WRITE THE CREATE SCRIPTS FOR ALL THREE TABLES
 CREATE TABLE DEPARTMENT(
-    id INT NOT NULL AUTO_INCREMENT
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
 );
+
+CREATE TABLE ROLES (
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(8,2),
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES DEPARTMENT(id)
+)
+
+CREATE TABLE EMPLOYEES (
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (manager_id) REFERENCES EMPLOYEES(id),
+    FOREIGN KEY (role_id) REFERENCES ROLES(id)
+)
+
 
 
 
